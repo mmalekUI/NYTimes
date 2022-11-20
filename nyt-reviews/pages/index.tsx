b51acb9c1705388383daa3a9708269c7f151b38d
mmalek <mmalek@griddynamics.com>
2022-11-20 17:46:24 +0100

    project setup

---
+++ nyt-reviews/pages/index.tsx
@@ -0,0 +1,8 @@
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <div>Movie-reviews</div>;
+}
